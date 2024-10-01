cbf33a7f02ed3693c31611e878ed90b23dab0843
klima3775 <113065590+klima3775@users.noreply.github.com>
2024-10-02 01:07:12 +0300

    add color ball

--- src/gameOptions.js
+++ src/gameOptions.js
@@ -5,6 +5,8 @@ const GameOptions = {
     width: 1920,
     height: 1080,
   },
+
+  gemColor: [0xff0000, 0x00ff00, 0x0000ff, 0xff00ff, 0xffff00, 0x00ffff],
   gameBackgroundColor: 0x222222,
   path: path,
   gemSpeed: 120,
