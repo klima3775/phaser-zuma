d66ba23da16c4cfe8b00671700f56f5fd7ab248c
Klima <klima3775@gmail.com>
2024-10-07 02:25:02 +0300

    Refactor Vite config files and update script source path in index.html

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -1,19 +1,45 @@
 import { defineConfig } from "vite";
 
+const phasermsg = () => {
+  return {
+    name: "phasermsg",
+    buildStart() {
+      process.stdout.write(`Building for production...\n`);
+    },
+    buildEnd() {
+      const line = "---------------------------------------------------------";
+      const msg = `❤️❤️❤️ Tell us about your game! - games@phaser.io ❤️❤️❤️`;
+      process.stdout.write(`${line}\n${msg}\n${line}\n`);
+
+      process.stdout.write(`✨ Done ✨\n`);
+    },
+  };
+};
+
 export default defineConfig({
-  base: "/zuma-phaser/", // Замените на имя вашего репозитория
+  base: "./",
+  logLevel: "warning",
   build: {
-    outDir: "dist", // Убедитесь, что выходная папка указана правильно
     rollupOptions: {
-      input: "./index.html", // Убедитесь, что путь к index.html указан правильно
       output: {
         manualChunks: {
           phaser: ["phaser"],
         },
       },
     },
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        passes: 2,
+      },
+      mangle: true,
+      format: {
+        comments: false,
+      },
+    },
   },
   server: {
     port: 8080,
   },
+  plugins: [phasermsg()],
 });
